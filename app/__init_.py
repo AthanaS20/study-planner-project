1daf861e62b3874e38e8b51dee957760de1f9f10
Athana Silva <athanasilva95@gmail.com>
2025-08-14 19:17:41 -0300

    Creating repository

---
+++ app/__init_.py
@@ -0,0 +1,12 @@
+from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dev-secret-key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///planner.db'
+
+db =SQLAlchemy(app)
+login_manager = LoginManager(app)
+
+from app import routes
